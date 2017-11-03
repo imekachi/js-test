bbe59f55c51bf12299dbdcc090b017f0f367c30c
imekachi <imekachi@gmail.com>
2017-11-03 11:06:25 +0700

    use normal function instead of arrow function

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
 import { expect, should } from 'chai'
 import { sum } from '../src'
 
-describe('sum', () => {
-  it('should return 10 when we pass 2 and 8', (done) => {
-    expect(sum(2+8)).to.be.equal(10)
+describe('sum', function () {
+  it('should return 10 when we pass 2 and 8', function (done) {
+    expect(sum(2 + 8)).to.be.equal(10)
     done()
   })
 })
