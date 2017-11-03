0205ecdb9c406bd7a71ad7c402643341d0d207fa
imekachi <imekachi@gmail.com>
2017-11-03 11:16:49 +0700

    add assert, should, expect syntax

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,30 @@
-import { expect, should } from 'chai'
+import chai, { expect, assert } from 'chai'
 import { sum } from '../src'
 
+// getting should, this is the way
+const should = chai.should()
+
 describe('sum', function () {
+  // BDD => {expect, should}
+  // TDD => {assert}
+  // It's all kinda all same, just choose the one that suits to you
+  // here are examples
+
+  // expect syntax
   it('should return 10 when we pass 2 and 8', function (done) {
     expect(sum(2 + 8)).to.be.equal(10)
+    done()
+  })
+
+  // should syntax
+  it('should return 8 when passing 10 and -2', function (done) {
+    sum(10, -2).should.be.equal(8)
+    done()
+  })
+
+  // assert syntax
+  it('should return -20 when passing -12 and -8', function (done) {
+    assert.equal(-20, sum(-12, -8))
     done()
   })
 })
